3102a216c2d80c1e455b7619bd5bfe1c7099fee4
Jo Mama <arcasti9@asu.edu>
2024-04-10 19:19:03 -0700

    Fixed an issue with the argument for the function I defined. Also rearranged the order of everything.

--- processImg.py
+++ processImg.py
@@ -5,6 +5,12 @@ import numpy as np
 import PIL
 from PIL import Image as Im
 
+# method to be used by other files in case we don't want to run this individually
+def imageTakeInner(image_file):
+    image = Im.open(image_file)
+    image = np.array(image)
+    return image
+
 # choose the image to use and the file to save to HERE
 image_file = 'stock-photo-young-attractive-dangerous-woman-aiming-at-gold-fish-55445056.jpg' # input file
 array_file = 'usable.npy' # 0utput file with the format .npy
@@ -12,9 +18,3 @@ array_file = 'usable.npy' # 0utput file with the format .npy
 image = Im.open(image_file)
 image = np.array(image)
 np.save(array_file, image)
-
-def imageTakeInner():
-    image_file
-    image = Im.open(image_file)
-    image = np.array(image)
-    return image

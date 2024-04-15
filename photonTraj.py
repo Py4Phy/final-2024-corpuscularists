5249a29cd93c7e354d29d8d233e6256dba93ff3e
Jo Mama <arcasti9@asu.edu>
2024-04-15 15:45:46 -0700

    Added parts for getting the pixel locations on the initial image and initializing the positions of the images

--- photonTraj.py
+++ photonTraj.py
@@ -3,5 +3,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import cm # Color maps
 from projFuncs import *
+from processImg import *
 
 ### Using geometrized units; c=G=1.
+
+pixel_length = 1
+z_position1 = 1000 # location of where we observe the final image
+z_position2 = -1000 # location of the initial image
+
+# set image position(s)
+initialImage = imageTakeInner('Epic_Redpilled_Beckstein.png')
+y_size, x_size, z_size = initialImage.shape()
+y_positions, x_positions = np.meshgrid(y_size, x_size)
+y_positions = pixel_length*(y_positions - y_size/2)
+x_positions = pixel_length*(x_positions - x_size/2)
+y_center = pixel_length*y_size/2
+x_center = pixel_length*x_size/2
--- processImg.py
+++ processImg.py
@@ -11,6 +11,17 @@ def imageTakeInner(image_file):
     image = np.array(image)
     return image
 
+# method to find where the ray passes through an image
+def findPixel(y_center, x_center, z_position, pixel_length, ri, v):
+    xi, yi, zi = ri # initial positions, should input the final positions BEFORE the ray passes through the image
+    vx, vy, vz = v # velocities from the same time as the initial position
+    Z = z_position - zi
+    xf = xi + Z*(vx/vz)
+    yf = yi + Z*(vy/vz)
+    j = (xf + x_center)/pixel_length
+    i = (yf + y_center)/pixel_length
+    return i, j # y then x indices for the pixel
+
 # choose the image to use and the file to save to HERE
 image_file = 'stock-photo-young-attractive-dangerous-woman-aiming-at-gold-fish-55445056.jpg' # input file
 array_file = 'usable.npy' # 0utput file with the format .npy
